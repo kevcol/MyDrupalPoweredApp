a8628910c2824fed76a95915422d6bdedc430b46
Kevin Colligan <kevin.colligan@macbookair-kevincolligan.local>
2017-01-02 17:41:34 -0800

    first commit

---
+++ MyDrupalPoweredApp/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  MyDrupalPoweredApp
+//
+//  Created by Kevin Colligan on 1/1/17.
+//  Copyright © 2017 Kevin Colligan. All rights reserved.
+//
+
+import UIKit
+import waterwheel
+import ObjectMapper
+
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    
+
+}
+
+
+
+
